d20d0a10f143b03186389d3373ea2b93c81fe4a9
ancucchi <ancucchi@gmail.com>
2025-07-16 16:28:54 +0200

    riga1
    riga2

---
+++ install and update.bat
@@ -0,0 +1,26 @@
+cls
+winget install --no-upgrade -e --id 7zip.7zip 
+winget install --no-upgrade -e --accept-package-agreements --id 9NBLGGH4QGHW				  &:: Microsoft Sticky Notes
+winget install --no-upgrade -e --id AntibodySoftware.WizTree 
+winget install --no-upgrade -e --id CodeSector.TeraCopy 
+winget install --no-upgrade -e --id CPUID.CPU-Z 
+winget install --no-upgrade -e --id Foxit.FoxitReader 
+winget install --no-upgrade -e --id geeksoftwareGmbH.PDF24Creator
+winget install --no-upgrade -e --id Google.Chrome 
+winget install --no-upgrade -e --id Mozilla.Firefox 
+winget install --no-upgrade -e --id Notepad++.Notepad++ 
+winget install --no-upgrade -e --id RevoUninstaller.RevoUninstaller
+winget install --no-upgrade -e --id TechPowerUp.GPU-Z 
+winget install --no-upgrade -e --id TheDocumentFoundation.LibreOffice 
+winget install --no-upgrade -e --id TheDocumentFoundation.LibreOffice.HelpPack 
+winget install --no-upgrade -e --id VideoLAN.VLC 
+winget install --no-upgrade -e --id voidtools.Everything 
+
+winget update
+
+@echo ---------------------------
+@echo digitare da  riga di comando 
+@echo ---------------------------
+@echo     winget update --all
+@echo ---------------------------
+@echo per aggiornare i software...
